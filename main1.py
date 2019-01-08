b769e99918a6aeb1dddaabbc92435c7ff230ba9b
Xinrui Lu <xinrui7@cs.ualberta.ca>
2019-01-08 10:39:55 -0700

    AAA

--- main1.py
+++ main1.py
@@ -7,10 +7,13 @@ import requests
 print(requests.__version__)
 
 r = requests.get("http://www.google.com")
-print(r.status_code)
-print(r.headers)
+#print(r.status_code)
+#print(r.headers)
 
+github_url = "https://raw.githubusercontent.com/xinruiLU/404-lab1/master/main1.py"
 
+r = requests.get(github_url)
+print(r.text)
 
 
 
